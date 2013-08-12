dc3fd48382777e529afc946c5e45cadbde72afee
Tobias Deekens <tobias.deekens@gmail.com>
2013-08-12 16:50:36 +0200

    Refactors index to use session module.

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
-session_start();
 setlocale(LC_ALL, "de_DE");
 date_default_timezone_set('Europe/Berlin');
 
 require_once('./composer/autoload.php');
 
 use Scandio\lmvc\LVC;
+use Scandio\lmvc\modules\session\Session;
+
+Session::start();
 
 LVC::initialize('config.json');
 LVC::dispatch();
