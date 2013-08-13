fcfa859415d39095341cc732e276a0bcef721b96
Tobias Deekens <tobias.deekens@gmail.com>
2013-08-13 15:06:09 +0200

    Removes session start call from app's index.

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ date_default_timezone_set('Europe/Berlin');
 require_once('./composer/autoload.php');
 
 use Scandio\lmvc\LVC;
-use Scandio\lmvc\modules\session\Session;
-
-Session::start();
 
 LVC::initialize('config.json');
 LVC::dispatch();
